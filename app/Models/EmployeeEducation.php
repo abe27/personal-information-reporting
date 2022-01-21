a3eb3649d5d67597eaa322bc01d0f10587453242
Taweechai Yuenyang <krumii.it@gmail.com>
2022-01-21 11:44:23 +0700

    แก้ไข model employee

--- app/Models/EmployeeEducation.php
+++ app/Models/EmployeeEducation.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class EmployeeEducation extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuids;
+
+    public $fillable = [
+        'profile_id',
+        'education_id',
+        'edu_from',
+        'edu_from_year',
+        'is_status',
+    ];
 }
